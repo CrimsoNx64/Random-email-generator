7e104423f3b70d16e7dac82d9f796950be56084b
CrimsoN <75698450+CrimsoNx64@users.noreply.github.com>
2022-09-27 17:54:49 +0100

    Add files via upload

---
+++ email gen.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    numbers=string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    result_str2 = ''.join(random.choice(numbers) for i in range(length))
+    result_str3=result_str+result_str2
+    result_str3=result_str3+"@gmail.com"
+    print(result_str3)
+
+get_random_string(8)
+get_random_string(8)
+get_random_string(8)
+get_random_string(8)
+get_random_string(8)
+
